7140fd2207bca8633aefd2e1f08d86076852a6e0
Bryan <bryanryu1@gmail.com>
2021-03-10 14:24:52 -0500

    install reacthookform, add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <h1 classname='Title'>Rapptr Labs</h1>
-      <h1>check here</h1>
+      <h1 classname="Title">Rapptr Labs</h1>
+      <div className='FormContainer'>
+      <form className="Form" onSubmit={handleSubmit(onSubmit)}>
+        <label>Email:</label>
+        <input name="email" placeholder="user@rapptrlabs.com" />
+        <label>Password:</label>
+        <input name="password" placeholder="Must be at least 4 characters" />
+      </form>
+      </div>
     </div>
   );
 }
