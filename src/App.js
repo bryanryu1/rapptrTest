2ad9dc0be9e42383371f816e46b13252426931b4
Bryan <bryanryu1@gmail.com>
2021-03-10 17:07:56 -0500

    create login with validations

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import { useForm } from "react-hook-form";
 
 function App() {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data, e) => {
+    console.log(data);
+    e.target.reset();
+  };
 
   return (
     <div className="App">
-      <h1 classname="Title">Rapptr Labs</h1>
-      <div className='FormContainer'>
-      <form className="Form" onSubmit={handleSubmit(onSubmit)}>
-        <label>Email:</label>
-        <input name="email" placeholder="user@rapptrlabs.com" />
-        <label>Password:</label>
-        <input name="password" placeholder="Must be at least 4 characters" />
-      </form>
+      <h1 className="Title">Rapptr Labs</h1>
+      <div className="FormContainer">
+        <form className="Form" onSubmit={handleSubmit(onSubmit)}>
+          <label>Email:</label>
+          <input
+            name="email"
+            placeholder="user@rapptrlabs.com"
+            ref={register({
+              required: true,
+              maxLength: 50,
+              validate: (value) => value.includes("@"),
+            })}
+          />
+          {errors.email && <p>Please enter a valid email</p>}
+          <label>Password:</label>
+          <input
+            name="password"
+            placeholder="Must be at least 4 characters"
+            ref={register({
+              required: true,
+              minLength: 4,
+              maxLength: 16,
+              validate: {
+                notSpace: (value) => value === value.trim(),
+              },
+            })}
+          />
+          <button>Login</button>
+        </form>
       </div>
     </div>
   );
