a75b50adf50ef026b87ebc3bc6a96473fdff368d
Bryan <bryanryu1@gmail.com>
2021-03-10 23:24:12 -0500

    add icons to input field

--- src/login.js
+++ src/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { IoPersonCircleSharp, IoLockClosedSharp } from "react-icons/io5";
 
 function Login() {
   const { register, handleSubmit, watch, errors } = useForm({
@@ -17,46 +18,56 @@ function Login() {
       <div className="FormContainer">
         <form className="Form" onSubmit={handleSubmit(onSubmit)}>
           <label>Email:</label>
-          <input
-            name="email"
-            placeholder="user@rapptrlabs.com"
-            style={{ border: errors.email ? "2px solid red" : "none" }}
-            ref={register({
-              required: true,
-              maxLength: 50,
-              validate: (value) => {
-                const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                return re.test(String(value).toLowerCase());
-              },
-            })}
-          />
+          <div className="InputWrapper">
+            <IoPersonCircleSharp color={"#2fccfc"} size={20} />
+            <input
+              className="Input"
+              name="email"
+              placeholder="user@rapptrlabs.com"
+              style={{ border: errors.email ? "2px solid red" : "none" }}
+              ref={register({
+                required: true,
+                maxLength: 50,
+                validate: (value) => {
+                  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                  return re.test(String(value).toLowerCase());
+                },
+              })}
+            />
+          </div>
           {errors.email && (
             <p className="ErrorMessage">Please enter a valid email</p>
           )}
           <label>Password:</label>
-          <input
-            name="password"
-            style={{ border: errors.password ? "2px solid red" : "none" }}
-            placeholder="Must be at least 4 characters"
-            ref={register({
-              required: true,
-              minLength: 4,
-              maxLength: 16,
-              validate: {
-                notSpace: (value) => value === value.trim(),
-              },
-            })}
-            type="password"
-          />
+          <div className="InputWrapper">
+            <IoLockClosedSharp color={"#2fccfc"} size={20} />
+            <input
+              className="Input"
+              name="password"
+              style={{ border: errors.password ? "2px solid red" : "none" }}
+              placeholder="Must be at least 4 characters"
+              ref={register({
+                required: true,
+                minLength: 4,
+                maxLength: 16,
+                validate: {
+                  notSpace: (value) => value === value.trim(),
+                },
+              })}
+              type="password"
+            />
+          </div>
           {errors.password && <p className="ErrorMessage">Invalid Password</p>}
-          <button
-            style={{
-              opacity: errors.email || errors.password ? "50%" : "100%",
-            }}
-            ref={register}
-          >
-            Login
-          </button>
+          <div className="buttonWrapper">
+            <button
+              style={{
+                opacity: errors.email || errors.password ? "50%" : "100%",
+              }}
+              ref={register}
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
     </div>
